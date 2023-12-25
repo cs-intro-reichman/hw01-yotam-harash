d9308d41da97e6e73e6756987e0a8c45d13a5244
yotamharash <154414629+yotamharash@users.noreply.github.com>
2023-12-25 09:51:27 +0200

    homework

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,25 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		System.out.println("hello");
+		
+				if (args.length < 2) {
+					System.out.println("Please provide two numbers as command line arguments.");
+					return;
+				}
+		
+				try {
+					int number1 = Integer.parseInt(args[0]);
+					int number2 = Integer.parseInt(args[1]);
+		
+					int sum = number1 + number2;
+					System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+				} catch (NumberFormatException e) {
+					System.out.println("Please provide valid integer numbers.");
+				}
+			
+		
+		
+         
 	}
 }
